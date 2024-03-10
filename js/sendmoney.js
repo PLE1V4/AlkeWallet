aa29c5b0360eb91367c134c8b691285f3b4733a9
Pablo Leiva <96198232+PLE1V4@users.noreply.github.com>
2024-03-10 19:58:15 -0300

    Agregado Manejo de Contactos
    Se agrega lista de contactos como radiobutton y se agrega funcionalidad para agregar contactos.

--- js/sendmoney.js
+++ js/sendmoney.js
@@ -4,9 +4,17 @@ let transactions = JSON.parse(localStorage.getItem('transactions'));
 
 const lblSaldo = $('#lblAvailable');
 const btnBack = $('.btnBack');
+const btnAddContact = $('#btnAddContact');
+const btnCerrarAdd = $('#btnCloseAdd');
 const formSent = $('#formSentMoney');
+const formAdd = $('#formAddContact');
 const cantidad = $('#txtSentMoney');
+const mensaje = $('#txtTransferMessage');
 const listaContactos = $('#divContactos');
+const divAgregarContacto = $('#divAddContact');
+const nombreContacto = $('#txtAddName');
+const cuentaContacto = $('#txtAddAccount');
+const aliasContacto = $('#txtAddAlias');
 
 btnBack.on('click', function () {
   $(location).attr('href', 'menu.html');
@@ -15,6 +23,19 @@ btnBack.on('click', function () {
 formSent.on('submit', function (event) {
   event.preventDefault();
   sentMoney();
+});
+
+formAdd.on('submit', function (event) {
+  event.preventDefault();
+  agregarContacto();
+});
+
+btnAddContact.on('click', function () {
+  divAgregarContacto.show();
+});
+
+btnCerrarAdd.on('click', function () {
+  divAgregarContacto.hide();
 });
 
 $(document).ready(function () {
@@ -41,19 +62,25 @@ function sentMoney() {
 }
 
 function cargarContactos() {
-  let tbody = '<table>';
-  contacts.forEach((element) => {
-    tbody +=
-      '<tr><label>' +
-      element.alias +
-      '</label><label>' +
-      element.nombre +
-      '</label><label>' +
-      element.cuenta +
-      '</label></tr>';
-  });
-  tbody += '</table>';
-  listaContactos.html(tbody);
+  let body = '';
+  if (contacts) {
+    contacts.forEach((element) => {
+      body +=
+        '<input type="radio" name="radContacts" value="' +
+        element.nombre +
+        '" id="' +
+        element.cuenta +
+        '"><label>' +
+        element.cuenta +
+        '</label><label>' +
+        element.alias +
+        '</label>';
+    });
+  } else {
+    body = '<span>No hay Contactos Registrados</span>';
+  }
+
+  listaContactos.html(body);
 }
 
 function registrarTransferencia(cantidad) {
@@ -62,9 +89,20 @@ function registrarTransferencia(cantidad) {
   localStorage.setItem('user', JSON.stringify(user));
   transactions.push({
     amount: cantidad,
-    desc: 'desc',
-    to: 'cuenta destino',
+    desc: mensaje.val(),
+    to: $("input[name='radContacts']:checked").val(),
     tipo: 1,
   });
   localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+function agregarContacto() {
+  contacts.push({
+    nombre: nombreContacto.val(),
+    cuenta: parseInt(cuentaContacto.val()),
+    alias: aliasContacto.val(),
+  });
+  cargarContactos();
+  $(`#${cuentaContacto.val()}`).prop('checked', true);
+  localStorage.setItem('contacts', JSON.stringify(contacts));
 }
